13b518846d4bd18353b4fe174240be067ddf67f5
Audrius Sveikauskas <audrius@sveikauskas.dev>
2023-01-04 13:52:46 +0200

    Preparing to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import MainLayout from './components/layouts/MainLayout';
 import HomePage from './pages/HomePage';
 
 const App = () => (
-  <BrowserRouter>
+  <HashRouter basename="/index.html">
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route index element={<HomePage />} />
       </Route>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 export default App;
